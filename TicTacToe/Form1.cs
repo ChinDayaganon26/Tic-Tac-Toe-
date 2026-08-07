ada65532aecfb90c2dc6459fa508d3ac110008e5
Chin chin Dayaganon <chinchindayaganon30@gmail.com>
2026-08-07 21:19:44 +0800

    Add placeholder Form1.cs (will replace with original Form1 later)

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TicTacToe
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        private void InitializeComponent()
+        {
+            // Placeholder: actual designer generated code copied from Codon25 will be added.
+        }
+
+        // The rest of Form1 code will be added from the original repo.
+    }
+}
