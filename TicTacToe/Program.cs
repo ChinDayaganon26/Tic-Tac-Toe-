9764918e623c92999fbcb91c1fddee9d5deaed79
Chin chin Dayaganon <chinchindayaganon30@gmail.com>
2026-08-07 22:07:04 +0800

    Program.cs

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -8,7 +8,8 @@ namespace TicTacToe
         [STAThread]
         static void Main()
         {
-            ApplicationConfiguration.Initialize();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
     }
