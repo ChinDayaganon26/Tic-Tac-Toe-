246a4032cfb515dd76926885ceae338dc64d045d
Chin chin Dayaganon <chinchindayaganon30@gmail.com>
2026-08-07 22:07:47 +0800

    Form1.cs

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -1,11 +1,26 @@
 using System;
+using System.Windows.Forms;
 
 namespace TicTacToe
 {
-    public partial class Form1 : System.Windows.Forms.Form
+    public partial class Form1 : Form
     {
-        private void InitializeComponent()
+        bool playerTurn = true; // true = X, false = O
+        int playerScore = 0;
+        int cpuScore = 0;
+
+        public Form1()
         {
-            
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Ihanda ang laro
+        }
+
+        // Dapat may Click event handler para sa bawat button!
+        // Halimbawa:
+        // private void button1_Click(object sender, EventArgs e) { ... }
     }
 }
